ca2ba397161b022001b7bc66dc94120f2ba96016
Cesar Emmanuel <cesaremmmanul@gmail.com>
2023-01-30 19:34:48 -0300

    feat: Criação do evento no Google

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs'
+import { google } from 'googleapis'
 import { NextApiHandler } from 'next'
 import { z } from 'zod'
+import { getGoogleOAuthToken } from '../../../../lib/google'
 import { prisma } from '../../../../lib/prisma'
 
 const CREATE_SCHEDULING_BODY = z.object({
@@ -48,13 +50,42 @@ const handler: NextApiHandler = async (req, res) => {
     return res.status(400).json({ error: 'Scheduling already exists' })
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       name,
       email,
       observations,
       date: schedulingDate.toDate(),
       user_id: user.id,
+    },
+  })
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: observations,
+      start: {
+        dateTime: schedulingDate.format(),
+      },
+      end: {
+        dateTime: schedulingDate.add(1, 'hour').format(),
+      },
+      attendees: [{ email, displayName: name }],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
     },
   })
 
